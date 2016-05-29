5dbd6008be6b64bfd5cb4a31df0e471b3725410e
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-05-29 16:16:56 -0500

    added Font

--- Sources/Cairo/Font.swift
+++ Sources/Cairo/Font.swift
@@ -7,6 +7,68 @@
 //
 
 import CCairo
+import CFontConfig
+
+public final class FontFace {
+    
+    // MARK: - Properties
+    
+    internal let internalPointer: OpaquePointer
+    
+    // MARK: - Initialization
+    
+    deinit {
+        
+        cairo_font_face_destroy(internalPointer)
+    }
+    
+    public init(fontConfigPattern: OpaquePointer) {
+        
+        self.internalPointer = cairo_ft_font_face_create_for_pattern(fontConfigPattern)!
+    }
+    
+    internal init(_ internalPointer: OpaquePointer) {
+        
+        self.internalPointer = internalPointer
+    }
+    
+    // MARK: - Accessors
+    
+    public var status: Status {
+        
+        return cairo_font_face_status(internalPointer)
+    }
+    
+    public lazy var type: cairo_font_type_t = cairo_font_face_get_type(self.internalPointer) // Never changes
+}
+
+public final class ScaledFont {
+    
+    // MARK: - Properties
+    
+    internal let internalPointer: OpaquePointer
+    
+    // MARK: - Initialization
+    
+    deinit {
+        
+        cairo_scaled_font_destroy(internalPointer)
+    }
+    
+    internal init(_ internalPointer: OpaquePointer) {
+        
+        self.internalPointer = internalPointer
+    }
+    
+    // MARK: - Accessors
+    
+    public var status: Status {
+        
+        return cairo_scaled_font_status(internalPointer)
+    }
+}
+
+// MARK: - Supporting Types
 
 public enum FontSlant: cairo_font_slant_t.RawValue {
     
