c792f1032ffc12e382350ef026538cb4bdabebd3
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-06-04 08:36:37 -0500

    updated Linux support

--- Package.swift
+++ Package.swift
@@ -3,7 +3,9 @@ import PackageDescription
 let package = Package(
     name: "Cairo",
     dependencies: [
-        .Package(url: "https://github.com/PureSwift/CCairo.git", majorVersion: 1)
+        .Package(url: "https://github.com/PureSwift/CCairo.git", majorVersion: 1),
+        .Package(url: "https://github.com/PureSwift/CFontConfig.git", majorVersion: 1),
+        .Package(url: "https://github.com/PureSwift/CFreeType.git", majorVersion: 1)
     ],
     targets: [
         Target(
