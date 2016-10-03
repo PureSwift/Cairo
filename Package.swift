c247d0b031c9b7b8b5c411d68acadfb7a086bb22
Tomas CW <tomascw@users.noreply.github.com>
2016-10-03 04:01:51 -0400

    Rearranged targets and dependencies in Package.swift
    "targets" after "dependencies" caused Swift 3's compiler to fail.

--- Package.swift
+++ Package.swift
@@ -2,14 +2,14 @@ import PackageDescription
 
 let package = Package(
     name: "Cairo",
+    targets: [
+        Target(
+            name: "Cairo")
+    ],
     dependencies: [
         .Package(url: "https://github.com/PureSwift/CCairo.git", majorVersion: 1),
         .Package(url: "https://github.com/PureSwift/CFontConfig.git", majorVersion: 1),
         .Package(url: "https://github.com/PureSwift/CFreeType.git", majorVersion: 1)
-    ],
-    targets: [
-        Target(
-            name: "Cairo")
     ],
     exclude: ["Xcode", "Sources/CairoUnitTests"]
 )
