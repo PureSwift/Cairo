ff5875d7094ce089ae701517ffcc23b600fda326
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-06-07 02:07:42 -0500

    Revert "Updated for Swift 4"
    This reverts commit 62655441aa2e966628f6d26cf81e9a120b369977.

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Cairo",
-            dependencies: ["CCairo", "CFontConfig", "CFreeType"]),
+            dependencies: []),
         .testTarget(
             name: "CairoTests",
             dependencies: ["Cairo"]),
