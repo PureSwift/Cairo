62655441aa2e966628f6d26cf81e9a120b369977
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-06-07 01:58:46 -0500

    Updated for Swift 4

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Cairo",
-            dependencies: []),
+            dependencies: ["CCairo", "CFontConfig", "CFreeType"]),
         .testTarget(
             name: "CairoTests",
             dependencies: ["Cairo"]),
