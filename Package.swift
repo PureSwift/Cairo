2e0a981dbe8d7599edd86fcc5544d66d1fd7e0ad
fwcd <fwcdmail@gmail.com>
2019-03-30 22:33:20 +0100

    Moved 'products' section above 'dependencies' as required

--- Package.swift
+++ Package.swift
@@ -5,13 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "Cairo",
+    products: [
+        .library(name: "Cairo", targets: ["Cairo"])
+    ],
     dependencies: [
         .package(url: "https://github.com/PureSwift/CCairo.git", .revision("2051a137b52995d7795c0dc9c08cd2ea2a2b570c")),
         .package(url: "https://github.com/PureSwift/CFontConfig.git", .revision("f47c3ea87307478428df762dc2e181c12643a884")),
         .package(url: "https://github.com/PureSwift/CFreeType.git", .revision("a8c830218999af31691e154cecae783bae1f0ab9"))
-    ],
-    products: [
-        .library(name: "Cairo", targets: ["Cairo"])
     ],
     targets: [
         .target(
