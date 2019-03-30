d5e12fa6eb43b8db8601bb908c2ed266098fd33d
fwcd <fwcdmail@gmail.com>
2019-03-30 21:56:50 +0100

    Update Package.swift to Swift 4.2

--- Package.swift
+++ Package.swift
@@ -1,17 +1,18 @@
-// swift-tools-version:3.0.2
+// swift-tools-version:4.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Cairo",
-    targets: [
-        Target(
-            name: "Cairo")
-    ],
     dependencies: [
-        .Package(url: "https://github.com/PureSwift/CCairo.git", majorVersion: 1),
-        .Package(url: "https://github.com/PureSwift/CFontConfig.git", majorVersion: 1),
-        .Package(url: "https://github.com/PureSwift/CFreeType.git", majorVersion: 1)
+        .package(url: "https://github.com/PureSwift/CCairo.git", from: "1.0.0"),
+        .package(url: "https://github.com/PureSwift/CFontConfig.git", from: "1.0.0"),
+        .package(url: "https://github.com/PureSwift/CFreeType.git", from: "1.0.0")
+    ],
+    targets: [
+        .target(
+            name: "Cairo"
+        )
     ]
 )
